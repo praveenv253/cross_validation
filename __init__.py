2c30eac96d40135421fb6b223996db4111c6f6b2
Praveen Venkatesh <praveenv253@gmail.com>
2017-06-11 22:49:54 -0400

    Add documentation to the cross_validate function.

--- __init__.py
+++ __init__.py
@@ -19,6 +19,33 @@ def cross_validate(cv_data, data_splitter, estimator, estimator_params,
     For e.g., for plain lasso, lamdas_list can simply be ([0.1, 1, 10], ).
     For elastic net, lamdas_list might be ([0.1, 1, 10], [0.1, 1, 10]) (for the
         two different lamdas corresponding to the l1 and l2 costs).
+
+    Returns
+    -------
+    lamda_stars : tuple of double
+        Values of the lamdas which minimize test error. lamda_star[i] is the
+        best value out of lamdas_list[i].
+    lamda_star_indices : tuple of int
+        Indices corresponding to the best values of lamdas.
+        lamda_star_indices[i] is the index of lamda_stars[i] in lamdas_list[i].
+    error : numpy array of shape (num_folds, num_lamdas)
+        For each fold, gives the error between the test data of that
+        fold and the training estimate of that fold, for every value of lamda
+    mean_error : numpy array of shape (num_lamdas,)
+        Mean of `error` over folds.
+    x_hats : numpy array of shape (num_sources, num_folds, num_lamdas)
+        Returned only if save_x_hats is True.
+        Value of the training estimate from each fold, for every value of
+        lamda.
+
+    Notes
+    -----
+    For the last three return values, note that num_lamdas is the product of
+    the lengths of each list in lamdas_list. lamdas_list is ravelled using
+    itertools.product, which means that the last lamda changes fastest. For
+    e.g., if 2 values of lamda_1, lamda_2 and lamda_3 are provided, then they
+    are ordered as 000, 001, 010, 011, 100, 101, 110, 111. A desired index can
+    be computed using np.unravel_index.
     """
 
     lamdas_sizes = tuple(lamdas.size for lamdas in lamdas_list)
