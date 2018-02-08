465de7f21356b3e7a946842aad63af037f3f0cf8
Praveen Venkatesh <praveenv253@gmail.com>
2018-02-07 19:51:27 -0500

    WIP: Rename files for coverage/consistency.

--- cross_validation/tests/__init__.py
+++ cross_validation/tests/__init__.py
@@ -1,2 +1,3 @@
 # This file needs to exist in order for pytest-cov to work.
 # See this: https://bitbucket.org/memedough/pytest-cov/issues/4/no-coverage-unless-test-directory-has-an
+from . import test_ds
