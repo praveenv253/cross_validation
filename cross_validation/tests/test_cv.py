265a546efa75ceb7b6fbc47c2b4f3d9bf1cbd4d8
Praveen Venkatesh <praveenv253@gmail.com>
2018-02-07 19:31:07 -0500

    Add tests for remaining data splitters.

--- cross_validation/tests/test_cv.py
+++ cross_validation/tests/test_cv.py
@@ -25,9 +25,37 @@ def test_k_fold():
     data = np.arange(10)
 
     for k in range(1, 11):
-        splitter = ds.KFold(10, k)
-        assert splitter.num_folds == k
-        for train, test in splitter:
-            agg_data = set(data[train]) | set(data[test])
-            assert agg_data == set(data)
-            assert len(agg_data) == 10
+        for randomize in (False, True):
+            splitter = ds.KFold(10, k, randomize)
+            assert splitter.num_folds == k
+
+            for train, test in splitter:
+                agg_data = set(data[train]) | set(data[test])
+                assert agg_data == set(data)
+                assert len(agg_data) == 10
+
+
+def test_leave_one_out():
+    data = np.arange(10)
+
+    splitter = ds.LeaveOneOut(10)
+    assert splitter.num_folds == 10
+
+    for train, test in splitter:
+        agg_data = set(data[train]) | set(data[test])
+        assert len(data[test]) == 1
+        assert agg_data == set(data)
+        assert len(agg_data) == 10
+
+
+def test_leave_one_in():
+    data = np.arange(10)
+
+    splitter = ds.LeaveOneIn(10)
+    assert splitter.num_folds == 10
+
+    for train, test in splitter:
+        agg_data = set(data[train]) | set(data[test])
+        assert len(data[train]) == 1
+        assert agg_data == set(data)
+        assert len(agg_data) == 10
